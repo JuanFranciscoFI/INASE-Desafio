888b508e253dc45323c857a65fe8d0c4c98616a1
Juan Francisco <jcuevas@fi.uba.ar>
2025-10-24 17:52:14 -0300

    add: entity to sample

---
+++ app/src/Model/Entity/Sample.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+class Sample extends Entity
+{
+    protected array $_accessible = [
+        'seal_number'   => true,
+        'company'       => true,
+        'species'       => true,
+        'seed_quantity' => true,
+
+        'id' => false,
+        '*'  => false,
+    ];
+}
